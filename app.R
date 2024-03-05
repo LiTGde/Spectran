a71efee1b7f820bf12f5c015479d9059117868c8
Johannes Zauner <112665672+JZauner@users.noreply.github.com>
2024-03-05 16:28:32 +0100

    Added automated deployment to shinyapps.io

--- app.R
+++ app.R
@@ -1,3 +1,5 @@
 pkgload::load_all(".")
-Spectran(lang_setting = "Deutsch", lang_link = TRUE)
+config_env <- Sys.getenv("CONFIG_ENV", "default")
+settings <- config::get(file = "_shiny_config.yml", config = config_env)
+Spectran(lang_setting = settings$language, lang_link = TRUE)
 
