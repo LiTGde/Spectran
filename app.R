36f49fa66386c9b5610c987b87690899452fb820
Johannes Zauner <112665672+JZauner@users.noreply.github.com>
2024-09-25 10:29:08 +0200

    testing automatic deployment

--- app.R
+++ app.R
@@ -1,6 +1,7 @@
 pkgload::load_all(".")
 # config_env <- Sys.getenv("CONFIG_ENV", "deutsch")
-config_env <- Sys.getenv("CONFIG_ENV")
-settings <- config::get(file = "_shiny_config.yml", config = config_env)
-Spectran(lang_setting = settings$language, lang_link = TRUE)
+# config_env <- Sys.getenv("CONFIG_ENV")
+# settings <- config::get(file = "_shiny_config.yml", config = config_env)
+# Spectran(lang_setting = settings$language, lang_link = TRUE)
+Spectran(lang_setting = Sys.getenv("CONFIG_ENV"), lang_link = TRUE)
 
